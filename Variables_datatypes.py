328c0049f6d9e305aba184f03d84c10be4fd446b
malathi22 <85837480+malathi22@users.noreply.github.com>
2021-06-16 10:32:20 +0530

    Day 4 task

---
+++ Variables_datatypes.py
@@ -0,0 +1,12 @@
+[16/06, 10:22 am] Malathi: num1=3
+num2=5.89
+num1=float(num1)
+num2=int(num2)
+print(num1,num2)
+[16/06, 10:22 am] Malathi: a=b=c=20
+a=a/10
+b=a*50
+c=c+60
+print(a,b,c)
+[16/06, 10:22 am] Malathi: string="m a n g o"
+print(string.replace("n","g"))
